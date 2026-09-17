263a8bc83d1c548779154d88cd2620e9a9bde539
mlpditto <medlifeplus@gmail.com>
2026-09-17 07:45:27 +0000

    Deploy: Update GitHub Pages [skip ci]

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-import { getDeptCategoryColor, getDeptPastelColor } from './colors.js?v=3.90';
+import { getDeptCategoryColor, getDeptPastelColor } from './colors.js?v=3.91';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, collection, query, where, getDocs, getDoc, setDoc, updateDoc, deleteDoc, doc, orderBy, addDoc, writeBatch } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -1065,6 +1065,11 @@ window.delSched = async (id) => {
         await deleteDoc(doc(db, "schedules", id));
         loadSchedules();
     }
+};
+
+window.openSpecialShift = () => {
+    const el = document.getElementById("specialShiftModal");
+    if (el) bootstrap.Modal.getOrCreateInstance(el).show();
 };
 
 window.openLeaveRequests = () => {
